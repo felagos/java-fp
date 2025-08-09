19e06194afdc6cfc1c564115862ec7b390953b28
Felipe Lagos <felagossanchez@gmail.com>
2025-08-09 18:24:42 -0400

    refactor: Replace main method implementation with testConsumer and testSupplier methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    private static void testConsumer() {
+        Consumer<String> consumer = (s) -> System.out.println(s);
+        consumer.accept("Hello, World!");
+    }
+
+    private static void testSupplier() {
+        Supplier<Double> randomNumberSupplier = () -> Math.random();
+        double randomNumber = randomNumberSupplier.get();
+        System.out.println("Random Number: " + randomNumber);
+    }
+
+    public static void main(String[] args) {
+        Main.testConsumer();
+        Main.testSupplier();
     }
 }
