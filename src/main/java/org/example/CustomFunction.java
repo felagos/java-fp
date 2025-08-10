d9a783e00c70f92f7c77e361123dd35759a1ff45
Felipe Lagos <felagossanchez@gmail.com>
2025-08-09 20:08:23 -0400

    feat: Add functional interfaces and implement test methods for function and predicate

---
+++ src/main/java/org/example/CustomFunction.java
@@ -0,0 +1,8 @@
+package org.example;
+
+@FunctionalInterface
+public interface CustomFunction {
+
+   String concatenate(String str1, String str2);
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class Main {
@@ -16,8 +17,30 @@ public class Main {
         System.out.println("Random Number: " + randomNumber);
     }
 
+    private static void testFunction() {
+        Function<Integer, String> intToString = (i) -> "Number: " + i;
+        String result = intToString.apply(42);
+        System.out.println("Result casting function: " + result);
+    }
+
+    private static void testPredicate() {
+        Predicate<Integer> isEven = (s) -> s % 2 == 0;
+        Boolean result = isEven.test(10);
+
+        System.out.println("Is 10 even? " + result);
+    }
+
+    private static void testCustomFunction() {
+        CustomFunction customFunction = (str1, str2) -> str1 + str2;
+        String result = customFunction.concatenate("Hello, ", "World!");
+        System.out.println("Concatenated String: " + result);
+    }
+
     public static void main(String[] args) {
         Main.testConsumer();
         Main.testSupplier();
+        Main.testFunction();
+        Main.testPredicate();
+        Main.testCustomFunction();
     }
 }
