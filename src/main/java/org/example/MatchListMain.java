3139d07ac49f3cad740259a4fdb6cf40559d9381
Felipe Lagos <felagossanchez@gmail.com>
2025-08-12 21:25:46 -0400

    feat: Update course data in MatchListMain and enhance review checks

--- src/main/java/org/example/MatchListMain.java
+++ src/main/java/org/example/MatchListMain.java
@@ -1,25 +1,28 @@
 package org.example;
 
 import java.util.List;
-
 import org.example.models.Course;
 
 public class MatchListMain {
 
     public static void main(String[] args) {
         var courses = List.of(
-            new Course("Java Programming", "Programming", 5, 100),
-            new Course("Python Programming", "Programming", 4, 80),
-            new Course("Web Development", "Development", 5, 120),
-            new Course("Data Science", "Data", 5, 150),
-            new Course("Machine Learning", "Data", 5, 200),
-            new Course("Cloud Computing", "Cloud", 5, 250),
-            new Course("Cybersecurity", "Security", 5, 300),
-            new Course("Blockchain Basics", "Blockchain", 5, 350)
-        );
+                new Course("Java Programming", "Programming", 5, 100),
+                new Course("Python Programming", "Programming", 1, 80),
+                new Course("Web Development", "Development", 5, 120),
+                new Course("Data Science", "Data", 5, 150),
+                new Course("Machine Learning", "Data", 3, 200),
+                new Course("Cloud Computing", "Cloud", 5, 250),
+                new Course("Cybersecurity", "Security", 5, 300),
+                new Course("Blockchain Basics", "Blockchain", 5, 350));
 
-        boolean hasAllCoursesHigherThan150 = courses.stream().allMatch(course -> course.numberOfStudents() > 150);
+        boolean hasAllCoursesHigherThan150 = courses.stream()
+                .allMatch(course -> course.numberOfStudents() > 150);
+        boolean areCoursesWithBadReviews = courses.stream()
+                .anyMatch(course -> course.reviewScore() <= 3);
+
         System.out.println("Has all courses with more than 150 students: " + hasAllCoursesHigherThan150);
+        System.out.println("Are there courses with bad reviews: " + areCoursesWithBadReviews);
 
     }
 
