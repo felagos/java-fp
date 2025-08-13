c131c327baacba90fc535d80c8bc466993ce5da0
Felipe Lagos <felagossanchez@gmail.com>
2025-08-12 21:40:24 -0400

    feat: Update Python Programming course data and enhance review checks

--- src/main/java/org/example/MatchListMain.java
+++ src/main/java/org/example/MatchListMain.java
@@ -8,7 +8,7 @@ public class MatchListMain {
     public static void main(String[] args) {
         var courses = List.of(
                 new Course("Java Programming", "Programming", 5, 100),
-                new Course("Python Programming", "Programming", 1, 80),
+                new Course("Python Programming", "Programming", 2, 80),
                 new Course("Web Development", "Development", 5, 120),
                 new Course("Data Science", "Data", 5, 150),
                 new Course("Machine Learning", "Data", 3, 200),
@@ -20,9 +20,12 @@ public class MatchListMain {
                 .allMatch(course -> course.numberOfStudents() > 150);
         boolean areCoursesWithBadReviews = courses.stream()
                 .anyMatch(course -> course.reviewScore() <= 3);
+        boolean areNotCoursesWithExcellentReviews = courses.stream()
+                .noneMatch(course -> course.reviewScore() == 7);
 
         System.out.println("Has all courses with more than 150 students: " + hasAllCoursesHigherThan150);
         System.out.println("Are there courses with bad reviews: " + areCoursesWithBadReviews);
+        System.out.println("Are not there courses with excellent reviews: " + areNotCoursesWithExcellentReviews);
 
     }
 
